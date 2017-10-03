f836b971ca0303211de52a0b3458afd23891363f
Zack Scholl <zack.scholl@gmail.com>
2017-10-03 14:56:46 -0400

    Vendoring

---
+++ vendor/github.com/schollz/collectlinks/collectlinks_test.go
@@ -0,0 +1,56 @@
+package collectlinks
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestAll(t *testing.T) {
+	reader := strings.NewReader(` <p>
+  <a href="http://motherfuckingwebsite.com">1</a>
+  <a href='http://news.google.com'>2</a>
+  <a style=\"\" href=http://imgur.com>3</a>
+  http://dontcollectthis.com
+</p>`)
+
+	links := All(reader)
+
+	fmt.Println(links)
+
+	if len(links) != 3 {
+		t.Error("Wrong number of links returned")
+	}
+
+	if links[0] != "http://motherfuckingwebsite.com" {
+		t.Error("The first link is incorrect")
+	}
+	if links[1] != "http://news.google.com" {
+		t.Error("The second link is incorrect")
+	}
+	if links[2] != "http://imgur.com" {
+		t.Error("The third link is incorrect")
+	}
+
+	reader = strings.NewReader(` <p>
+  <a href="/#recipes/some-recipe">1</a>
+  <a href='/another-test'>2</a>
+  <a style=\"\" href=http://imgur.com>3</a>
+  http://dontcollectthis.com
+</p>`)
+
+	links = All(reader)
+
+	fmt.Println(links)
+
+	if len(links) != 3 {
+		t.Error("Wrong number of links returned")
+	}
+
+	if links[0] != "/#recipes/some-recipe" {
+		t.Error("The first link is incorrect")
+	}
+	if links[1] != "/another-test" {
+		t.Error("The second link is incorrect")
+	}
+}
