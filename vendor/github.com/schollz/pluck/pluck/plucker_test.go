d6c52b892f93ac54a94e139ddb988c9ae9fb198b
Zack Scholl <zack.scholl@gmail.com>
2017-10-09 07:52:51 -0500

    Update dependencies

--- vendor/github.com/schollz/pluck/pluck/plucker_test.go
+++ vendor/github.com/schollz/pluck/pluck/plucker_test.go
@@ -15,6 +15,27 @@ func BenchmarkParseFile(b *testing.B) {
 	}
 }
 
+func BenchmarkParseFileStream(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		p, _ := New()
+		p.Verbose(false)
+		p.Load("test/config.toml")
+		p.PluckFile("test/test.txt", true)
+	}
+}
+
+func TestPluck0(t *testing.T) {
+	p, _ := New()
+	p.Verbose(false)
+	err := p.Load("test/config.toml")
+	if err != nil {
+		t.Error(err)
+	}
+	err = p.PluckFile("test/test.txt")
+	if err != nil {
+		t.Error(err)
+	}
+}
 func TestPluck1(t *testing.T) {
 	p, err := New()
 	p.Verbose(false)
@@ -53,6 +74,7 @@ func TestPluck1(t *testing.T) {
     ]
 }`, p.ResultJSON(true))
 
+	p, _ = New()
 	p.Load("test/food.toml")
 	p.PluckURL("http://www.foodnetwork.com/recipes/food-network-kitchen/15-minute-shrimp-tacos-with-spicy-chipotle-slaw-3676441")
 	assert.Equal(t, `15-Minute Shrimp Tacos with Spicy Chipotle Slaw Recipe | Food Network Kitchen | Food Network`, p.Result()["title"])
@@ -64,6 +86,46 @@ func TestPluck1(t *testing.T) {
 	})
 	p.PluckString("XaZbYcZd")
 	assert.Equal(t, `c`, p.Result()["0"])
+}
+
+func TestPluck1Stream(t *testing.T) {
+	p, err := New()
+	p.Verbose(false)
+	if err != nil {
+		t.Error(err)
+	}
+	err = p.Load("test/config.toml")
+	if err != nil {
+		t.Error(err)
+	}
+	assert.Equal(t, 3, len(p.pluckers))
+	assert.Equal(t, "0", p.pluckers[0].config.Name)
+	assert.Equal(t, -1, p.pluckers[0].config.Limit)
+	assert.Equal(t, "options", p.pluckers[1].config.Name)
+	assert.Equal(t, "songs", p.pluckers[2].config.Name)
+
+	p.PluckFile("test/test.txt", true)
+	assert.Equal(t, `{
+    "0": "Category Archives: Song of the Day Podcast",
+    "options": [
+        "2009 Countdown",
+        "2010 Countdown",
+        "2011 Countdown"
+    ],
+    "songs": [
+        "Juana Molina \u0026#8211; Cosoco",
+        "Charms – Siren",
+        "Daddy Issues \u0026#8211; Locked Out",
+        "Cloud Control \u0026#8211; Rainbow City",
+        "Kevin Morby \u0026#8211; Come to Me Now",
+        "Les Big Byrd \u0026#8211; Two Man Gang",
+        "Thunderpussy – Velvet Noose",
+        "Captain, We\u0026#8217;re Sinking \u0026#8211; Trying Year",
+        "Mammút – “The Moon Will Never Turn On Me”",
+        "Songhoy Blues \u0026#8211; Voter"
+    ]
+}`, p.ResultJSON(true))
+
 }
 
 func TestPluck2(t *testing.T) {
@@ -157,8 +219,7 @@ func TestPluckCutSection(t *testing.T) {
 		Deactivator: `"`,
 		Finisher:    "Section 3",
 	})
-	err = p.PluckString(`
-<h1>Section 1</h1>
+	err = p.PluckString(`<h1>Section 1</h1>
 <a href="link1">1</a>
 <a href="link2">2</a>
 <h1>Section 2</h1>
@@ -166,8 +227,7 @@ func TestPluckCutSection(t *testing.T) {
 <a href="link4">4</a>
 <h1>Section 3</h1>
 <a href="link5">5</a>
-<a href="link6">6</a>
-`)
+<a href="link6">6</a>`)
 	assert.Nil(t, err)
 	assert.Equal(t, `{"0":["link3","link4"]}`, p.ResultJSON())
 
